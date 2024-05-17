0f4b39a2e8d105faf905182ec6f033993c994ec0
lipa13 <jaklip04@gmail.com>
2024-05-17 16:05:14 +0200

    added abstract class Graph

---
+++ Headers/Graph/Graph.h
@@ -0,0 +1,25 @@
+#ifndef AIZO_PROJEKT_2_GRAPH_H
+#define AIZO_PROJEKT_2_GRAPH_H
+
+#include <string>
+
+using namespace std;
+
+class Graph
+{
+protected:
+    int V; // ilość wierzchołków
+    int E; // ilość krawędzi
+
+public:
+    Graph();
+    virtual void addEdge(int start, int end, int weight) = 0;
+    virtual int* getNeighbors(int vertex) = 0;
+    virtual void display() = 0;
+    virtual void loadFromFile(string& filename) = 0;
+    virtual void generateRandom(int vertices, int d) = 0;
+    int getV();
+    int getE();
+};
+
+#endif //AIZO_PROJEKT_2_GRAPH_H
---
+++ Sources/Graph/Graph.cpp
@@ -0,0 +1,15 @@
+#include "../../Headers/Graph/Graph.h"
+
+using namespace std;
+
+Graph::Graph():V(0), E(0) {}
+
+int Graph::getV()
+{
+    return V;
+}
+
+int Graph::getE()
+{
+    return E;
+}
