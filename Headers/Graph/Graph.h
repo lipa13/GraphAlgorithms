793fb8a6adf06c1dc5e14953216df10891ae7501
lipa13 <jaklip04@gmail.com>
2024-05-17 18:42:00 +0200

    removed class Edge and replaced it by struct Edge inside the Graph class

--- Headers/Graph/Graph.h
+++ Headers/Graph/Graph.h
@@ -3,8 +3,6 @@
 
 #include <string>
 
-#include "../Edge.h"
-
 using namespace std;
 
 class Graph
@@ -14,9 +12,19 @@ protected:
     int numE; // ilość krawędzi
 
 public:
+    struct Edge
+    {
+        int startV;
+        int endV;
+        int weight;
+
+        Edge(): startV(0), endV(0), weight(0){}
+        Edge(int start, int end, int w): startV(start), endV(end), weight(w){}
+    };
+
     Graph();
     virtual void addEdge(int start, int end, int weight) = 0;
-    virtual int* getNeighbors(int V) = 0;
+    virtual Edge* getNeighbors(int V) = 0;
     virtual void display() = 0;
     virtual void loadFromFile(string& path) = 0;
     virtual void generateRandom(int vertices, int d) = 0;
--- Sources/Edge.cpp
+++
@@ -1,5 +0,0 @@
-#include "../Headers/Edge.h"
-
-Edge::Edge():startV(0), endV(0), weight(0){}
-
-Edge::Edge(int start, int end, int w):startV(start), endV(end), weight(w){}
