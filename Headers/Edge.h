119c8f86772d5222aa445c9a0d7c3ed30792d13f
lipa13 <jaklip04@gmail.com>
2024-05-17 17:53:09 +0200

    added Edge class

---
+++ Headers/Edge.h
@@ -0,0 +1,14 @@
+#ifndef AIZO_PROJEKT_2_EDGE_H
+#define AIZO_PROJEKT_2_EDGE_H
+
+class Edge
+{
+    int startV;
+    int endV;
+    int weight;
+
+    Edge();
+    Edge(int start, int end, int w);
+};
+
+#endif //AIZO_PROJEKT_2_EDGE_H
---
+++ Sources/Edge.cpp
@@ -0,0 +1,5 @@
+#include "../Headers/Edge.h"
+
+Edge::Edge():startV(0), endV(0), weight(0){}
+
+Edge::Edge(int start, int end, int w):startV(start), endV(end), weight(w){}
