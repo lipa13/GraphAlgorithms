425d01cea1103a87d40db9c59e3bc5ca23136e4c
lipa13 <jaklip04@gmail.com>
2024-05-17 17:32:30 +0200

    changed arguments name

--- Headers/Graph/Graph.h
+++ Headers/Graph/Graph.h
@@ -16,7 +16,7 @@ public:
     virtual void addEdge(int start, int end, int weight) = 0;
     virtual int* getNeighbors(int vertex) = 0;
     virtual void display() = 0;
-    virtual void loadFromFile(string& filename) = 0;
+    virtual void loadFromFile(string& path) = 0;
     virtual void generateRandom(int vertices, int d) = 0;
     int getV();
     int getE();
