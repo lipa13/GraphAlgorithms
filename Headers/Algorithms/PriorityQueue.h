34d7a877e1ec8d3ed727f37f4f1e02d9448f65bc
lipa13 <jaklip04@gmail.com>
2024-06-08 22:42:27 +0200

    added comments

--- Headers/Algorithms/PriorityQueue.h
+++ Headers/Algorithms/PriorityQueue.h
@@ -1,32 +1,34 @@
 #ifndef AIZO_PROJEKT_2_PRIORITYQUEUE_H
 #define AIZO_PROJEKT_2_PRIORITYQUEUE_H
 
+// struktura reprezentująca węzeł kopca
 struct HeapNode
 {
-    int v;
-    int key;
+    int v; // wierzchołek
+    int key; // klucz (priorytet)
 };
 
+// klasa reprezentująca kolejkę priorytetową w postaci kopca
 class PriorityQueue
 {
-    HeapNode* queue;
-    int* vPos;
-    int currentSize;
-    int maxSize;
-    void fix_down(int p);
-    void fix_up(int i);
+    HeapNode* queue; // tablica reprezentująca kolejkę priorytetową
+    int* vPos; // tablica pomocnicza przechowująca pozycje wierzchołków w kolejce
+    int currentSize; // aktualny rozmiar kolejki
+    int maxSize; // maksymalny rozmiar kolejki
+    void fix_down(int p); // metoda naprawy kopca w dół
+    void fix_up(int i); // metoda naprawy kopca w górę
 
 public:
-    void init();
+    void init(); // inicjalizacja kolejki priorytetowej
     PriorityQueue(int s);
-    void clear();
+    void clear(); // czyszczenie kolejki priorytetowej
     ~PriorityQueue();
 
-    void insertKey(int v, int key);
-    void decreaseKey(int v, int key);
-    HeapNode extractMin();
-    bool isInQueue(int v) const;
-    bool isEmpty() const;
+    void insertKey(int v, int key); // wstawienie klucza do kolejki
+    void decreaseKey(int v, int key); // zmniejszenie klucza w kolejce
+    HeapNode extractMin(); // wyciągnięcie minimalnego klucza z kolejki
+    bool isInQueue(int v) const; // sprawdzenie, czy wierzchołek jest w kolejce
+    bool isEmpty() const; // sprawdzenie, czy kolejka jest pusta
 };
 
 #endif //AIZO_PROJEKT_2_PRIORITYQUEUE_H
--- Sources/Algorithms/PriorityQueue.cpp
+++ Sources/Algorithms/PriorityQueue.cpp
@@ -29,57 +29,65 @@ PriorityQueue::~PriorityQueue()
 
 void PriorityQueue::fix_up(int i)
 {
-    while(i && queue[i].key<queue[(i-1)/2].key)
+    // naprawa kopca w górę, aby zachować właściwości kopca
+    while(i>0 && queue[i].key<queue[(i-1)/2].key)
     {
+        // aktualizowanie pozycji wierzchołków w kopcu
         vPos[queue[i].v] = (i-1)/2;
         vPos[queue[(i-1)/2].v] = i;
-        swap(queue[i], queue[(i-1)/2]);
-        i = (i-1)/2;
+        swap(queue[i], queue[(i-1)/2]); // zamiana elementów kopca
+        i = (i-1)/2; // aktualizacja wartości indeksu
     }
 }
 
 void PriorityQueue::insertKey(int v, int key)
 {
-    currentSize++;
+    // wstawienie nowego klucza do kolejki
+    currentSize++; // zwiększenie aktualnego rozmiaru
+    // wstawienie klucza na ostatnie miejsce w kolejce
     int i = currentSize-1;
     queue[i] = {v,key};
     vPos[v] = i;
 
-    fix_up(i);
+    fix_up(i); // naprawa kopca, żeby ustawić nowy klucz w odpowiednim miejscu
 }
 
 void PriorityQueue::decreaseKey(int v, int key)
 {
+    // zmniejszenie klucza dla danego wierzchołka
     int i = vPos[v];
     queue[i].key = key;
 
+    // naprawa kopca w górę
     fix_up(i);
 }
 
 HeapNode PriorityQueue::extractMin()
 {
+    // wyciągnięcie minimalnego elementu z kopca
     if(currentSize==0)
     {
-        return{-1, numeric_limits<int>::max()};
+        return{-1, numeric_limits<int>::max()}; // zwrócenie maksymalnej wartości int, jeśli kopiec jest pusty
     }
 
     HeapNode root = queue[0];
-    swap(queue[0], queue[currentSize-1]);
-    vPos[queue[0].v] = 0;
-    vPos[queue[currentSize-1].v]=currentSize-1;
-    currentSize--;
+    swap(queue[0], queue[currentSize-1]); // zamiana korzenia z ostatnim elementem
+    vPos[queue[0].v] = 0; // aktualizacja zapisanej pozycji dla nowego korzenia
+    vPos[queue[currentSize-1].v]=currentSize-1; // aktualizacja zapisanej pozycji dla węzła, który usuwamy z kolejki
+    currentSize--; // zmniejszenie aktualnego rozmiaru kolejki
 
-    fix_down(0);
+    fix_down(0); // naprawa kopca w dół
 
-    return root;
+    return root; // zwrócenie minimalnego węzła, który usunęliśmy z kolejki
 }
 
 void PriorityQueue::fix_down(int p)
 {
-    int parent = p;
-    int left = 2*p+1;
-    int right = 2*p+2;
+    int parent = p; // przypisanie wartości indeksu rodzica
+    int left = 2*p+1; // obliczenie indeksu lewego potomka
+    int right = 2*p+2; // obliczenie indeksu prawego potomka
 
+    // jeśli mieścimy się w zakresie i key ordzica jest wieksze od potomka to indeks rodzica staje się indeksem potomka
     if(left<currentSize && queue[left].key<queue[parent].key)
     {
         parent = left;
@@ -90,6 +98,7 @@ void PriorityQueue::fix_down(int p)
         parent = right;
     }
 
+    // jęsli indeks rodzica się zmieniamy pozycje z określonym potomkiem i przywracamy właściwosci kopca
     if(parent!=p)
     {
         vPos[queue[parent].v] = p;
